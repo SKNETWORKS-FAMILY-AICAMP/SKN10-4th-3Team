f73b6d47746d075b04a09d1567c69026191fa62e
HoneyWater8 <kjyjyh8@gmail.com>
2025-05-08 19:50:20 +0900

    코드 몇개 지움

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .models import Message
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import JsonResponse
 
 # Create your views here.
 
