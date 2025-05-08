c8ce635c2fb5fabd64aea560e15a777d5f9a041d
syofbb26 <cube1426@gmail.com>
2025-05-08 21:10:03 +0900

    session api 추가

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Session(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 class Message(models.Model):
+    # session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='messages')  # 이 줄 중요!!
     sender = models.CharField(max_length=255)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import show_all_messages
+# from .views import show_all_messages
+from .views import (
+    # show_all_messages,
+    list_sessions,
+    create_session,
+    get_messages_by_session,
+    delete_session,
+)
+
 
 urlpatterns = [
-    path('show_all_messages/', show_all_messages, name='chatbot-show_all_messages'),
+    # path('show_all_messages/', show_all_messages, name='chatbot-show_all_messages'),
+    path('sessions/', list_sessions),  # GET
+    path('sessions/create/', create_session),  # POST
+    path('sessions/<int:session_id>/messages/', get_messages_by_session),  # GET
+    path('sessions/<int:session_id>/delete/', delete_session),  # DELETE
 ]
